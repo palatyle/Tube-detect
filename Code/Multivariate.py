cdb860ea0cc59dde566cc41605fcf810ead97381
Tyler Paladino <palatyle@isu.edu>
2022-04-05 15:56:51 -0600

    file reading and lin reg. tests

--- Code/Multivariate.py
+++ Code/Multivariate.py
@@ -74,3 +74,52 @@ def apply_no_data_val(ds, no_data):
     '''
     return np.ma.masked_equal(ds,no_data)
 
+def read_thermal_band(fn):
+    src_GDAL = GDAL_read_tiff(fn)
+    band = src_GDAL.GetRasterBand(6).ReadAsArray()
+    nodata = get_no_data_val(src_GDAL)
+
+    # Convert GDAL raster dataset to a numpy array
+    
+    return apply_no_data_val(band, nodata)
+
+def read_in_tiff_data(fn):
+    src_GDAL = GDAL_read_tiff(fn)
+
+    # Get no data value
+    nodata = get_no_data_val(src_GDAL)
+
+    # Convert GDAL raster dataset to a numpy array
+    src_NP = GDAL2NP(src_GDAL)
+
+    # Apply the no data value to the entire numpy arr
+    return apply_no_data_val(src_NP, nodata)
+
+start_time = time.time()
+
+NDVI_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\NDVI.tif'
+NDVI = read_in_tiff_data(NDVI_fn)
+
+NDWI_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\NDWI.tif'
+NDWI = read_in_tiff_data(NDWI_fn)
+
+MSAVI2_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\MSAVI2.tif'
+MSAVI2 = read_in_tiff_data(MSAVI2_fn)
+
+slope_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\DEM_slope.tif'
+slope = read_in_tiff_data(slope_fn)
+
+aspect_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\DEM_aspect.tif'
+aspect = read_in_tiff_data(aspect_fn)
+
+hs_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\DEM_hs.tif'
+hs = read_in_tiff_data(hs_fn)
+
+thermal_fn = 'C:\\Students\\Paladino\\Tube-detect\\data\\day_ortho_16bit.tif'
+thermal = read_thermal_band(thermal_fn)
+# Create regression object
+reg = LinearRegression()
+
+reg.fit(NDVI,thermal)
+print("done")
+print("--- %s seconds ---" % (time.time()-start_time))
