e53ea91902e4a1f73002b576b2f0849d78942f3f
caspheat <caspheat@isu.edu>
2022-05-16 15:10:13 -0600

    Started loop and calculations

--- Code/Albedo_Stats.py
+++ Code/Albedo_Stats.py
@@ -1,26 +1,79 @@
-# %matplotlib inline             I commented this out because the internet said I could replace it with lines 3-4.
 import matplotlib.pyplot as plt
-new_obj.resample('M').sum().plot(kind="bar")
-plt.show()
 import numpy as np
-import os 
+import os
+from osgeo import gdal
 import PIL
 from PIL import Image
 
-for i in range(1, 21):
-    col = int(255 * i / 20)
-    img = Image.new('RGB', (120, 80), color=(col, col, col))
-    img.save('%02d.bmp' % i, format='bmp')
 
+'''
 ims = []
-for i in range(1, 21):
-    ims.append(Image.open('%02d.bmp' % i, mode='r'))
+ims.append(Image.open("D:\Data\HHA_Calculated_Albedo\AlbedoS2A_MSIL2A_20190511T181921_N0212_R127_T12TUP_20190511T224452_super_resolved.tif"))
+'''
 
-ims[0] # 1, -1  out
 
-ims = np.array([np.array(im) for im in ims])
-ims.shape #out
-imave = np.average(ims,axis=0)
-imave.shape #out
-result = Image.fromarray(imave.astype('uint8'))
-result.save('result.bmp')
+def GDAL_read_tiff(fn):
+    '''
+    Returns GDAL raster object
+    Parameters
+    ----------
+    fn: Full directory and filename of .tiff 
+    Returns
+    -------
+    raster: GDAL raster object
+    '''
+    raster = gdal.Open(fn)
+    return raster
+
+def GDAL2NP(raster):
+    '''
+    Returns N dimensional numpy array of GDAL raster object
+    Parameters
+    ----------
+    raster: GDAL raster object
+    Returns
+    -------
+    raster_NP: raster numpy array
+    '''
+    print("Convert to numpy array...")
+    raster_NP = raster.ReadAsArray()
+    print("Done!")
+    return raster_NP    
+
+
+'''
+first_file = GDAL_read_tiff("E:\Data\HHA_Calculated_Albedo\AlbedoS2A_MSIL2A_20190511T181921_N0212_R127_T12TUP_20190511T224452_super_resolved.tif")
+first_raster = GDAL2NP(first_file)
+
+second_file = GDAL_read_tiff("E:\Data\HHA_Calculated_Albedo\AlbedoS2A_MSIL2A_20190720T181931_N0213_R127_T12TUP_20190721T001757_super_resolved.tif")
+second_raster = GDAL2NP(second_file)
+
+print("done")
+
+test = np.stack([first_raster, second_raster])
+
+test_average = np.average(test, axis=0)
+
+test_std = np.std(test, axis = 0)
+print("nice")
+'''
+
+
+
+
+working_directory = "E:\Data\HHA_Calculated_Albedo"
+os.chdir(working_directory)
+working_list = os.listdir(working_directory)
+
+for eachfile in working_list:
+    each_gdal = GDAL_read_tiff(eachfile)
+    each_raster = GDAL2NP(each_gdal)
+
+    print("rasters initalized")
+
+    full_stack = np.stack(each_raster, axis = 0)  #i saw the .shape online, dunno if I need it
+
+    print("done!")
+
+average = np.average(full_stack, axis = 0)
+std_dev = np.std(full_stack, axis = 0)
