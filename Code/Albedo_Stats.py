1e6cd0f42dd6559e2b6f89bb3e44394d59d6051f
caspheat <caspheat@isu.edu>
2022-05-18 15:38:15 -0600

    Did the Stats Correctly!

--- Code/Albedo_Stats.py
+++ Code/Albedo_Stats.py
@@ -2,14 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from osgeo import gdal
+from osgeo.gdalnumeric import CopyDatasetInfo
 import PIL
 from PIL import Image
-
-
-'''
-ims = []
-ims.append(Image.open("D:\Data\HHA_Calculated_Albedo\AlbedoS2A_MSIL2A_20190511T181921_N0212_R127_T12TUP_20190511T224452_super_resolved.tif"))
-'''
 
 
 def GDAL_read_tiff(fn):
@@ -40,6 +35,56 @@ def GDAL2NP(raster):
     print("Done!")
     return raster_NP    
 
+def write_band(raster_GDAL, band, dest_dir, out_fn):
+    '''
+    Returns None. Writes raster band to disk
+    Parameters
+    ----------
+    raster: rasterio object 
+    band: Band array to write
+    dest_dir: Destination directory 
+    out_fn: Output filename
+    '''
+
+    if band.dtype == 'int16':
+        print('Writing tif...')
+        # band = band.filled(fill_value=10001)
+        
+        driver = gdal.GetDriverByName("GTiff")
+        
+        dsOut = driver.Create(os.path.join(dest_dir, out_fn), raster_GDAL.RasterXSize, raster_GDAL.RasterYSize, 1, gdal.GDT_Int16, options=["COMPRESS=LZW"])
+        CopyDatasetInfo(raster_GDAL,dsOut)
+        dsOut.GetRasterBand(1).WriteArray(band)
+        dsOut.GetRasterBand(1).SetNoDataValue(-32767)
+        # dsOut.GetRasterBand(1).SetNoDataValue(np.nan)
+        dsOut.FlushCache()
+    elif band.dtype == 'uint16':
+       
+        band = band.filled(fill_value=65535)
+        print('Writing tif...')
+        driver = gdal.GetDriverByName("GTiff")
+        
+        dsOut = driver.Create(os.path.join(dest_dir, out_fn), raster_GDAL.RasterXSize, raster_GDAL.RasterYSize, 1, gdal.GDT_UInt16, options=["COMPRESS=LZW"])
+        CopyDatasetInfo(raster_GDAL,dsOut)
+        dsOut.GetRasterBand(1).WriteArray(band)
+        dsOut.GetRasterBand(1).SetNoDataValue(65535)
+        dsOut.FlushCache()
+
+    elif band.dtype == "float32":
+        print('Writing tif...')
+        driver = gdal.GetDriverByName("GTiff")
+        
+        dsOut = driver.Create(os.path.join(dest_dir, out_fn), raster_GDAL.RasterXSize, raster_GDAL.RasterYSize, 1, gdal.GDT_Float32, options=["COMPRESS=LZW"])
+        CopyDatasetInfo(raster_GDAL,dsOut)
+        dsOut.GetRasterBand(1).WriteArray(band)
+        dsOut.FlushCache()
+
+
+    dsOut=None
+
+
+    return None 
+
 
 '''
 first_file = GDAL_read_tiff("E:\Data\HHA_Calculated_Albedo\AlbedoS2A_MSIL2A_20190511T181921_N0212_R127_T12TUP_20190511T224452_super_resolved.tif")
@@ -64,16 +109,32 @@ print("nice")
 working_directory = "E:\Data\HHA_Calculated_Albedo"
 os.chdir(working_directory)
 working_list = os.listdir(working_directory)
+array_list = []
 
 for eachfile in working_list:
     each_gdal = GDAL_read_tiff(eachfile)
     each_raster = GDAL2NP(each_gdal)
 
-    print("rasters initalized")
+    # listtoarray_raster = np.array(each_raster)
+    array_list.append(each_raster)
 
-    full_stack = np.stack(each_raster, axis = 0)  #i saw the .shape online, dunno if I need it
+    print("rasters initalized")
 
     print("done!")
 
+
+full_stack = np.stack(array_list, axis = 0)  #i saw the .shape online, dunno if I need it
 average = np.average(full_stack, axis = 0)
 std_dev = np.std(full_stack, axis = 0)
+print("Done!")
+
+fig, ax = plt.subplots()
+img_plot = ax.imshow(std_dev)
+fig.colorbar(img_plot)
+ax.set_xlabel("x distance")
+ax.set_ylabel("y diatance")
+ax.set_title("Standard Deviation, Hell's Half Acre")
+
+plt.show()
+
+write_band(each_gdal, average, "E:\Data\Georeference_Outputs", "Average.tiff" )
