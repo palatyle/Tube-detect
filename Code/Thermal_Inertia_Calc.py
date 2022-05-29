1b61e06f186ddccf72caae9dc8fd2bc60064d7ad
caspheat <caspheat@isu.edu>
2022-05-29 00:24:04 -0600

    Started TIcalc and made decent progress. Questionz

--- Code/Thermal_Inertia_Calc.py
+++ Code/Thermal_Inertia_Calc.py
@@ -51,3 +51,32 @@ def get_solar_azi_alt(dt,lat,lon):
 
     # Get solar azimuth and altitude
     s_alt,s_azi = get_solar_azi_alt(flight_dt_tz_aware,tube_lat,tube_lon)
+
+
+angular_frequency = 7.2921159*10^(-5)
+day_sat_time = # insert input to the function--overpass time
+night_sat_time = # insert input to the function-- overpass time
+day_temp = # temperature at time day_sat_time
+night_temp = # temperature at time night_sat_time
+t_max = #??? Ask Tyler and find out because t_max and t_min (with the small t's) are not defined in the paper.
+t_min = #?
+
+Tmax = day_sat_time +((day_temp - night_temp)*[(np.cos(angular_frequency*t_max)) -
+ (np.cos(angular_frequency*day_sat_time))])/((np.cos(angular_frequency*day_sat_time)) -(np.cos(angular_frequency*night_sat_time)))
+
+Tmin = night_sat_time+((day_temp - night_temp)*[(np.cos(angular_frequency*t_min)) -
+ (np.cos(angular_frequency*night_sat_time))])/((np.cos(angular_frequency*day_sat_time))-(np.cos(angular_frequency*night_sat_time)))
+
+
+temp_change = abs(Tmax -Tmin)
+
+
+albedo = average #need to get this from Albedo_Stats or my files and properly format it with Tyler's help
+ATI = (1-albedo)/temp_change
+
+
+
+thermal_inertia = 
+
+
+
