5837b7b2888dc485d79e438a4752e08d5e8c64d0
Tyler Paladino <palatyle@isu.edu>
2022-08-16 11:40:14 -0600

    Made script multi-os friendly

--- Code/Multivariate.py
+++ Code/Multivariate.py
@@ -10,31 +10,34 @@ import seaborn as sns
 
 start_time = time.time()
 
-NDVI_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\NDVI.tif.csv", header=None)
+# upper_dir = "C:\\Users\\***REMOVED***\\Downloads\\"
+upper_dir = "/Users/tylerpaladino/Downloads/"
+
+NDVI_df = pd.read_csv(os.path.join(upper_dir, "NDVI.tif.csv"), header=None)
 NDVI_df = NDVI_df.replace(-32767,np.nan)
 print(len(NDVI_df))
 
-NDWI_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\NDWI.tif.csv",header=None)
+NDWI_df = pd.read_csv(os.path.join(upper_dir, "NDWI.tif.csv"),header=None)
 NDWI_df = NDWI_df.replace(-32767,np.nan)
 print(len(NDWI_df))
 
-thermal_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\thermal.tif.csv",header=None)
+thermal_df = pd.read_csv(os.path.join(upper_dir, "thermal.tif.csv"),header=None)
 thermal_df = thermal_df.replace(65535,np.nan)
 print(len(thermal_df))
 
-slope_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\DEM_slope.tif.csv",header=None)
+slope_df = pd.read_csv(os.path.join(upper_dir, "DEM_slope.tif.csv"),header=None)
 slope_df = slope_df.replace(-9999.,np.nan)
 print(len(slope_df))
 
-roughness_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\DEM_roughness.tif.csv",header=None)
+roughness_df = pd.read_csv(os.path.join(upper_dir, "DEM_roughness.tif.csv"),header=None)
 roughness_df = roughness_df.replace(-9999.,np.nan)
 print(len(roughness_df))
 
-aspect_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\DEM_aspect.tif.csv",header=None)
+aspect_df = pd.read_csv(os.path.join(upper_dir, "DEM_aspect.tif.csv"),header=None)
 aspect_df = aspect_df.replace(-9999.,np.nan)
 print(len(aspect_df))
 
-hillshade_df = pd.read_csv("C:\\Users\\***REMOVED***\\Downloads\\DEM_hs.tif.csv",header=None)
+hillshade_df = pd.read_csv(os.path.join(upper_dir, "DEM_hs.tif.csv"),header=None)
 hillshade_df = hillshade_df.replace(0,np.nan)
 print(len(hillshade_df))
 
@@ -61,14 +64,14 @@ hillshade_df=pd.DataFrame
 
 
 
-g = sns.pairplot(full_df,corner=True,plot_kws={'alpha':0.2})
-print('Done with seaborn plot command')
-g.savefig("C:\\Users\\***REMOVED***\\Downloads\\seaborn_scatter.png")
-print("done plotting scatter")
-print("--- %s seconds ---" % (time.time()-start_time))
+# g = sns.pairplot(full_df,corner=True,plot_kws={'alpha':0.2})
+# print('Done with seaborn plot command')
+# g.savefig("C:\\Users\\***REMOVED***\\Downloads\\seaborn_scatter.png")
+# print("done plotting scatter")
+# print("--- %s seconds ---" % (time.time()-start_time))
 
-g2 = sns.pairplot(full_df,kind="kde",corner=True)
-print('Done with seaborn plot command:kde')
-g2.savefig("C:\\Users\\***REMOVED***\\Downloads\\seaborn_kde.png")
-print("done plotting kde")
-print("--- %s seconds ---" % (time.time()-start_time))
+# g2 = sns.pairplot(full_df,kind="kde",corner=True)
+# print('Done with seaborn plot command:kde')
+# g2.savefig("C:\\Users\\***REMOVED***\\Downloads\\seaborn_kde.png")
+# print("done plotting kde")
+# print("--- %s seconds ---" % (time.time()-start_time))
