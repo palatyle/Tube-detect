d38bdfddb4f6fbe091eb29c5f518f8ac4f03b2ec
Tyler Paladino <palatyle@isu.edu>
2022-08-16 12:23:49 -0600

    Simplified code and added in preliminary regression functionality

--- Code/Multivariate.py
+++ Code/Multivariate.py
@@ -13,39 +13,44 @@ start_time = time.time()
 # upper_dir = "C:\\Users\\***REMOVED***\\Downloads\\"
 upper_dir = "/Users/tylerpaladino/Downloads/"
 
-NDVI_df = pd.read_csv(os.path.join(upper_dir, "NDVI.tif.csv"), header=None)
-NDVI_df = NDVI_df.replace(-32767,np.nan)
-print(len(NDVI_df))
+def read_data(filename,no_data_val):
+    """Reads in dataframe from csv file. Replaces no data values with nans. 
 
-NDWI_df = pd.read_csv(os.path.join(upper_dir, "NDWI.tif.csv"),header=None)
-NDWI_df = NDWI_df.replace(-32767,np.nan)
-print(len(NDWI_df))
+    Args:
+        filename (str): Full filename of csv file to read in.
+        no_data_val (int or float): no data value to replace with nan.
 
-thermal_df = pd.read_csv(os.path.join(upper_dir, "thermal.tif.csv"),header=None)
-thermal_df = thermal_df.replace(65535,np.nan)
-print(len(thermal_df))
+    Returns:
+        df: dataframe with no data values replaced with nans.
+    """    
+    df = pd.read_csv(filename,header=None)
+    df.replace(no_data_val,np.nan,inplace=True)
+    return df 
 
-slope_df = pd.read_csv(os.path.join(upper_dir, "DEM_slope.tif.csv"),header=None)
-slope_df = slope_df.replace(-9999.,np.nan)
-print(len(slope_df))
+# Read in dataframes
+NDVI_df = read_data(os.path.join(upper_dir+"NDVI.tif.csv"),-32767)
 
-roughness_df = pd.read_csv(os.path.join(upper_dir, "DEM_roughness.tif.csv"),header=None)
-roughness_df = roughness_df.replace(-9999.,np.nan)
-print(len(roughness_df))
+NDWI_df = read_data(os.path.join(upper_dir, "NDWI.tif.csv"),-32767)
 
-aspect_df = pd.read_csv(os.path.join(upper_dir, "DEM_aspect.tif.csv"),header=None)
-aspect_df = aspect_df.replace(-9999.,np.nan)
-print(len(aspect_df))
+thermal_df = read_data(os.path.join(upper_dir, "thermal.tif.csv"),65535)
 
-hillshade_df = pd.read_csv(os.path.join(upper_dir, "DEM_hs.tif.csv"),header=None)
-hillshade_df = hillshade_df.replace(0,np.nan)
-print(len(hillshade_df))
+slope_df = read_data(os.path.join(upper_dir, "DEM_slope.tif.csv"),-9999.)
+
+roughness_df = read_data(os.path.join(upper_dir, "DEM_roughness.tif.csv"),-9999.)
+
+aspect_df = read_data(os.path.join(upper_dir, "DEM_aspect.tif.csv"),-9999.)
+
+hillshade_df = read_data(os.path.join(upper_dir, "DEM_hs.tif.csv"),0)
 
 
+# Concatenate all the dataframes
 full_df = pd.concat([NDVI_df, NDWI_df, slope_df, roughness_df, aspect_df, hillshade_df, thermal_df],axis=1)
 full_df.columns = ["NDVI","NDWI","slope","roughness","aspect","hillshade","thermal"]
 
+# Drop any rows that contain nans. 
 full_df.dropna(inplace=True)
+
+# Delete dataframes and run garbage collector to free up memory
 del [[NDVI_df, NDWI_df, thermal_df, slope_df, roughness_df, aspect_df, hillshade_df]]
 gc.collect()
 NDVI_df=pd.DataFrame
@@ -56,7 +61,10 @@ roughness_df=pd.DataFrame
 aspect_df=pd.DataFrame
 hillshade_df=pd.DataFrame
 
-
+regr = linear_model.LinearRegression()
+regr.fit(full_df[["NDVI","NDWI","slope","roughness","aspect","hillshade"]],full_df["thermal"])
+regr.score(full_df[["NDVI","NDWI","slope","roughness","aspect","hillshade"]],full_df["thermal"])
+print(regr.coef_)
 # fig,ax=plt.subplots()
 # pd.plotting.scatter_matrix(full_df,ax=ax)
 # print('Done with pandas plot')
@@ -64,11 +72,12 @@ hillshade_df=pd.DataFrame
 
 
 
-# g = sns.pairplot(full_df,corner=True,plot_kws={'alpha':0.2})
-# print('Done with seaborn plot command')
-# g.savefig("C:\\Users\\***REMOVED***\\Downloads\\seaborn_scatter.png")
-# print("done plotting scatter")
-# print("--- %s seconds ---" % (time.time()-start_time))
+g = sns.pairplot(full_df,corner=True,plot_kws={'alpha':0.2})
+g.map_lower(sns.regplot,scatter=False,line_kws={'color':'red','lw':1,'alpha':0.5})
+print('Done with seaborn plot command')
+g.savefig(os.path.join(upper_dir,"seaborn_scatter.png"))
+print("done plotting scatter")
+print("--- %s seconds ---" % (time.time()-start_time))
 
 # g2 = sns.pairplot(full_df,kind="kde",corner=True)
 # print('Done with seaborn plot command:kde')
